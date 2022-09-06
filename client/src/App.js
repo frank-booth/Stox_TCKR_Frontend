83d69fac063a996390476d53cff59d273d0c8ca9
frank-booth <jfraley519@gmail.com>
2022-09-06 09:22:39 -0400

    react app installed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  )
+}
+
+export default App
