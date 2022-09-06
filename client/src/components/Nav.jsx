169a23b662c610d3f4f30e4870162d29119de530
frank-booth <jfraley519@gmail.com>
2022-09-06 12:38:54 -0400

    nav bar design begun

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,22 +2,20 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <NavLink className="links" to="/">
-            Home
-          </NavLink>
-          <NavLink className="links" to="/economic_data">
-            Economic Data
-          </NavLink>
-          <NavLink className="links" to="/news">
-            News
-          </NavLink>
-          <NavLink className="links" to="/profile">
-            Profile
-          </NavLink>
-        </div>
+    <div className="nav-bar">
+      <div className="nav-links">
+        <NavLink className="links" to="/">
+          Home
+        </NavLink>
+        <NavLink className="links" to="/economic_data">
+          Economic Data
+        </NavLink>
+        <NavLink className="links" to="/news">
+          News
+        </NavLink>
+        <NavLink className="links" to="/profile">
+          Profile
+        </NavLink>
       </div>
     </div>
   )
