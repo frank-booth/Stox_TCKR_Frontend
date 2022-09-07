d4deba19d6760c8a3e0babc5bcb66dfb33542383
frank-booth <jfraley519@gmail.com>
2022-09-07 10:58:16 -0400

    forms generated

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,13 @@ import Profile from './pages/Profile'
 import EconomicData from './pages/EconomicData'
 import News from './pages/News'
 import Login from './components/Login'
+import SignUp from './components/SignUp'
 
 function App() {
+  const API_KEY = process.env.REACT_APP_ALPHA_KEY
+
   const [users, setUsers] = useState()
+  const [cpiData, setCpiData] = useState()
 
   const getAllUsers = async () => {
     let res = await axios.get(`${BASE_URL}/users`)
@@ -19,9 +23,21 @@ function App() {
     setUsers(res.data)
   }
 
-  useEffect(() => {
-    getAllUsers()
-  }, [])
+  // useEffect(() => {
+  //   getAllUsers()
+  // }, [])
+
+  const getCpiData = async () => {
+    let res = await axios.get(
+      `https://www.alphavantage.co/query?function=CPI&interval=monthly&apikey=${API_KEY}`
+    )
+    console.log(res.data)
+    setCpiData(res.data)
+  }
+
+  //  useEffect(() => {
+  //     getCpiData()
+  //   }, [])
 
   return (
     <div className="App">
@@ -34,7 +50,8 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/economic_data" element={<EconomicData />} />
           <Route path="/news" element={<News />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login users={users} />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </main>
     </div>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,50 @@
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
-  return <div></div>
+  let navigate = useNavigate()
+
+  const signUp = () => {
+    navigate('/signup')
+  }
+
+  return (
+    <div>
+      <Segment placeholder>
+        <Grid columns={2} relaxed="very" stackable>
+          <Grid.Column>
+            <Form>
+              <Form.Input
+                icon="user"
+                iconPosition="left"
+                label="Username"
+                placeholder="Username"
+              />
+              <Form.Input
+                icon="lock"
+                iconPosition="left"
+                label="Password"
+                type="password"
+              />
+
+              <Button content="Login" primary />
+            </Form>
+          </Grid.Column>
+
+          <Grid.Column verticalAlign="middle">
+            <Button
+              content="Sign up"
+              icon="signup"
+              size="big"
+              onClick={signUp}
+            />
+          </Grid.Column>
+        </Grid>
+
+        <Divider vertical>Or</Divider>
+      </Segment>
+    </div>
+  )
 }
 
 export default Login
