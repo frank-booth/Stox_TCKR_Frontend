931104df4936c61d07f054b25eb55fa58834bbac
frank-booth <jfraley519@gmail.com>
2022-09-07 09:01:34 -0400

    successful axios call to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import './App.css'
 import { BASE_URL } from './globals'
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Nav from './components/Nav'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import EconomicData from './pages/EconomicData'
 import News from './pages/News'
+import Login from './components/Login'
 
 function App() {
+  const [users, setUsers] = useState()
+
+  const getAllUsers = async () => {
+    let res = await axios.get(`${BASE_URL}/users`)
+    console.log(res.data)
+    setUsers(res.data)
+  }
+
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +34,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/economic_data" element={<EconomicData />} />
           <Route path="/news" element={<News />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
 const Home = () => {
   return (
     <div>
       <h1>Hello</h1>
+      <NavLink className="home-links" to="/login">
+        Login
+      </NavLink>
     </div>
   )
 }
