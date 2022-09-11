c4155e79b7ca7d9a2d19cacd2c6190a17fb7ff23
frank-booth <jfraley519@gmail.com>
2022-09-11 17:34:48 -0400

    search function working

--- client/src/pages/News.jsx
+++ client/src/pages/News.jsx
@@ -1,41 +1,52 @@
-import { Search, Grid } from 'semantic-ui-react'
+import { Search, Grid, Input, Form, Button } from 'semantic-ui-react'
 import { useState } from 'react'
 import axios from 'axios'
 
 const News = () => {
   const API_KEY2 = process.env.REACT_APP_ALPHA_KEY
-  let searchQuery = ''
   const [searchData, setSearchData] = useState()
+  const [searchQuery, setSearchQuery] = useState('')
 
-  const handleSearchChange = async (e) => {
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setSearchQuery(e.target.value)
+  }
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    let res = await axios.get(
-      `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${searchQuery}&topics=technology&apikey=${API_KEY2}`
-    )
-    console.log(res.data)
-    setSearchData(res.data)
+    // let res = await axios.get(
+    //   `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${searchQuery}&topics=technology&apikey=${API_KEY2}`
+    // )
+    // console.log(res.data)
+    // setSearchData(res.data)
+    setSearchQuery('')
+    console.log(searchQuery)
   }
 
-  //  useEffect(() => {
-  //     getCpiData()
-  //   }, [])
+  // const handleKeyPress = (e) => {
+  //   //it triggers by pressing the enter key
+  //   if (e.keyCode === 13) {
+  //     handleSubmit()
+  //   }
+  // }
+
   return (
     <div>
       <Grid>
         <Grid.Column width={6}>
-          <Search
-            // loading={loading}
-            // onResultSelect={(e, data) =>
-            //   dispatch({
-            //     type: 'UPDATE_SELECTION',
-            //     selection: data.result.title
-            //   })
-            // }
-            onSearchChange={handleSearchChange}
-            // resultRenderer={resultRenderer}
-            // results={results}
-            value={searchQuery}
-          />
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              id="searchQuery"
+              type="text"
+              icon="search"
+              iconPosition="left"
+              placeholder="Search"
+              onChange={handleChange}
+              value={searchQuery}
+
+              // onKeyPress={handleKeyPress}
+            />
+            <Button type="submit">Search</Button>
+          </Form>
         </Grid.Column>
       </Grid>
     </div>
